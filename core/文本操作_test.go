f61ab53ca553264aa9fa3206e16746a6e391bf0e
csuilong <csuilong@163.com>
2020-04-11 07:51:32 +0800

    ok

--- core/文本操作_test.go
+++ core/文本操作_test.go
@@ -5,3 +5,8 @@ import "testing"
 func TestE删全部空(t *testing.T) {
 	t.Log("E删全部空", E删全部空("我 爱 e f u n 好   棒"))
 }
+func TestE子文本替换(t *testing.T) {
+
+	t.Log("TestE子文本替换", E子文本替换("作    者：颜温","作    者：",""))
+}
+
