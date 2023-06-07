be15bc60860aecc8ee236ff1003649f8165fe649
duolabmeng6 <1715109585@qq.com>
2023-06-07 15:27:09 +0800

    修复bug

--- egin/Middleware/BasicAuthMiddleware.go
+++ egin/Middleware/BasicAuthMiddleware.go
@@ -1,6 +1,9 @@
 package Middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"strconv"
+)
 
 // BasicAuthMiddleware http基本认证中间件
 //
@@ -15,17 +18,19 @@ func BasicAuthMiddleware(realm string, username, password string) gin.HandlerFun
 		// 从请求头获取用户名和密码
 		reqUsername, reqPassword, ok := c.Request.BasicAuth()
 		if !ok {
+			realm = strconv.Quote(realm)
 			// 没有提供认证信息,返回401
-			c.AbortWithStatus(401)
 			c.Writer.Header().Set("WWW-Authenticate", "Basic realm="+realm)
+			c.AbortWithStatus(401)
+
 			return
 		}
 
 		// 验证用户名和密码
 		if reqUsername != username || reqPassword != password {
 			// 用户名或密码错误
-			c.AbortWithStatus(401)
 			c.Writer.Header().Set("WWW-Authenticate", "Basic realm="+realm)
+			c.AbortWithStatus(401)
 			return
 		}
 
