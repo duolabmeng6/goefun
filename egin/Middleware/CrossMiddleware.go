a5f84a8d02627e74a307eb346e902790780951f4
多啦b梦 <1715109585@qq.com>
2024-11-07 09:02:03 +0800

    Update CrossMiddleware.go

--- egin/Middleware/CrossMiddleware.go
+++ egin/Middleware/CrossMiddleware.go
@@ -5,12 +5,16 @@ import "github.com/gin-gonic/gin"
 // CrossMiddleware 跨域中间件
 func CrossMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// 设置允许跨域的Header
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		c.Header("Access-Control-Allow-Headers", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 
-		// 继续处理请求
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
 		c.Next()
 	}
 }
